a27e80d57b0ffd5536ccd01fa6562f80bf4b97f5
Hannes Jansen van Rensburg <jpijansenvanrensburg@gmail.com>
2024-08-14 17:53:47 +0200

    Added Methods for Calculation History

--- CalculatorLibrary/CalculatorLibrary.cs
+++ CalculatorLibrary/CalculatorLibrary.cs
@@ -5,6 +5,8 @@ namespace CalculatorLibrary
     public class Calculator
     {
         JsonWriter writer;
+
+        static List<double> calculationHistory = new List<double>();
         public Calculator()
         {
             StreamWriter logFile = File.CreateText("calculatorlog.json");
@@ -30,14 +32,17 @@ namespace CalculatorLibrary
                 case "a":
                     result = num1 + num2;
                     writer.WriteValue("Add");
+                    calculationHistory.Add(result);
                     break;
                 case "s":
                     result = num1 - num2;
                     writer.WriteValue("Subtract");
+                    calculationHistory.Add(result);
                     break;
                 case "m":
                     result = num1 * num2;
                     writer.WriteValue("Multiply");
+                    calculationHistory.Add(result);
                     break;
                 case "d":
                     // Ask the user to enter a non-zero divisor.
@@ -45,6 +50,7 @@ namespace CalculatorLibrary
                     {
                         result = num1 / num2;
                         writer.WriteValue("Divide");
+                        calculationHistory.Add(result);
                     }
                     break;
                 // Return text for an incorrect option entry.
@@ -63,5 +69,39 @@ namespace CalculatorLibrary
             writer.WriteEndObject();
             writer.Close();
         }
+
+        static void ViewHistory()
+        {
+            if (calculationHistory.Count == 0)
+            {
+                Console.WriteLine("No calculations in history");
+                return;
+            }
+
+            Console.WriteLine("\nCalculation History:");
+            for (int i = 0; i < calculationHistory.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {calculationHistory[i]}");
+            }
+        }
+        static void DeleteFromHistory()
+        {
+            ViewHistory();
+
+            if (calculationHistory.Count == 0)
+                return;
+
+            Console.Write("\nEnter the number of the calculation to delete: ");
+            if (int.TryParse(Console.ReadLine(), out int index) && index > 0 && index <= calculationHistory.Count)
+            {
+                calculationHistory.RemoveAt(index - 1);
+                Console.WriteLine("Calculation deleted.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. No calculation deleted.");
+            }
+        }
+
     }
 }
