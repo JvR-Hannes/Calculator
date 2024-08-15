04a04c0bb181552905ccd6cef4e10eff6a2136d1
Hannes Jansen van Rensburg <jpijansenvanrensburg@gmail.com>
2024-08-15 13:04:00 +0200

    Cleaned up code and fixed issues after debugging

--- CalculatorLibrary/CalculatorLibrary.cs
+++ CalculatorLibrary/CalculatorLibrary.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using System.Text.RegularExpressions;
 
 namespace CalculatorLibrary
 {
@@ -19,6 +20,7 @@ namespace CalculatorLibrary
         }
         public double DoOperation(double num1, double num2, string op)
         {
+            
             double result = double.NaN;
             
             writer.WriteStartObject();
@@ -27,35 +29,35 @@ namespace CalculatorLibrary
             writer.WritePropertyName("Operand2");
             writer.WriteValue(num2);
             writer.WritePropertyName("Operation");
-            
+
             switch (op)
             {
                 case "a":
                     result = num1 + num2;
-                    writer.WriteValue("Add");
-                    calculationHistory.Add(result.ToString());
                     break;
                 case "s":
                     result = num1 - num2;
-                    writer.WriteValue("Subtract");
-                    calculationHistory.Add(result.ToString());
                     break;
                 case "m":
                     result = num1 * num2;
-                    writer.WriteValue("Multiply");
-                    calculationHistory.Add(result.ToString());
                     break;
                 case "d":
                     if (num2 != 0)
                     {
                         result = num1 / num2;
-                        writer.WriteValue("Divide");
-                        calculationHistory.Add(result.ToString());
+                    }
+                    else
+                    {
+                        Console.WriteLine("Division by Zero is not allowed.");
                     }
                     break;
                 default:
+                    Console.WriteLine("Invalid Operation.");
                     break;
             }
+            string calculation = $"{num1}{op}{num2} = {result}";
+            writer.WriteValue(op == "+" ? "Add" : op == "-" ? "Subtract" : op == "*" ? "Multiply" : "Divide");
+            calculationHistory.Add(calculation);
             writer.WritePropertyName("Result");
             writer.WriteValue(result);
             writer.WriteEndObject();
@@ -80,7 +82,7 @@ namespace CalculatorLibrary
             Console.WriteLine("\nCalculation History:");
             for (int i = 0; i < calculationHistory.Count; i++)
             {
-                Console.WriteLine($"{i + 1}. {calculationHistory[i]}");
+                Console.WriteLine($"{i + 1}{calculationHistory[i]}");
             }
         }
         public static void DeleteFromHistory()
@@ -104,25 +106,25 @@ namespace CalculatorLibrary
         public static void ShowMenu()
         {
             Console.WriteLine("Console Calculator in C#\r");
-            Console.WriteLine("------------------------\n");
+            Console.WriteLine("------------------------");
 
             Console.WriteLine("Calculator Menu:");
             Console.WriteLine("1. Perform a new calculation");
             Console.WriteLine("2. Perform Trigonometry");
-            Console.WriteLine("2. Calculate Ten Power");
-            Console.WriteLine("2. Calculate To Power");
-            Console.WriteLine("2. Calculate Square Root");
-            Console.WriteLine(". View calculation history");
-            Console.WriteLine(". Delete a calculation from history");
-            Console.WriteLine(". Reuse a calculation from history");
-            Console.WriteLine(". Exit");
+            Console.WriteLine("3. Calculate Ten Power");
+            Console.WriteLine("4. Calculate To Power");
+            Console.WriteLine("5. Calculate Square Root");
+            Console.WriteLine("6. View calculation history");
+            Console.WriteLine("7. Delete a calculation from history");
+            Console.WriteLine("8. Reuse a calculation from history");
+            Console.WriteLine("9. Exit");
             Console.Write("Choose an option: ");
             string? option = Console.ReadLine();
 
             switch (option)
             {
                 case "1":
-                    //TODO: Use the Method that performs a calculation
+                    PerformCalculation();
                     break;
                 case "2":
                     PerformTrigonometry();
@@ -153,6 +155,76 @@ namespace CalculatorLibrary
                     break;
             }
         }
+
+        public static void PerformCalculation()
+        {
+            string? numInput1 = "";
+            string? numInput2 = "";
+            double result = 0;
+
+            Console.Write("Type a number, and then press Enter: ");
+            numInput1 = Console.ReadLine();
+
+            double cleanNum1 = 0;
+            while (!double.TryParse(numInput1, out cleanNum1))
+            {
+                Console.Write("This is not valid input. Please enter a numeric value: ");
+                numInput1 = Console.ReadLine();
+            }
+
+            Console.Write("Type another number, and then press Enter: ");
+            numInput2 = Console.ReadLine();
+
+            double cleanNum2 = 0;
+            while (!double.TryParse(numInput2, out cleanNum2))
+            {
+                Console.Write("This is not valid input. Please enter a numeric value: ");
+                numInput2 = Console.ReadLine();
+            }
+
+            Console.WriteLine("Choose an operator from the following list:");
+            Console.WriteLine("\ta - Add");
+            Console.WriteLine("\ts - Subtract");
+            Console.WriteLine("\tm - Multiply");
+            Console.WriteLine("\td - Divide");
+            Console.Write("Your option? ");
+
+            string? op = Console.ReadLine();
+
+            if (op == null || !Regex.IsMatch(op, "[a|s|m|d]"))
+            {
+                Console.WriteLine("Error: Unrecognized input.");
+            }
+            else
+            {
+                try
+                {
+                    Calculator calculator = new Calculator();
+                    result = calculator.DoOperation(cleanNum1, cleanNum2, op);
+                    if (double.IsNaN(result))
+                    {
+                        Console.WriteLine("This operation will result in a mathematical error.\n");
+                    }
+                    else Console.WriteLine("Your result: {0:0.##}\n", result);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Oh no! An exception occurred trying to do the math.\n - Details: " + e.Message);
+                }
+            }
+            Console.WriteLine("------------------------\n");
+
+            Console.Write("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
+            if (Console.ReadLine() == "n")
+            {
+                Console.WriteLine("Goodbye");
+                Console.ReadKey();
+                Environment.Exit(1);
+            } 
+
+            Console.WriteLine("\n");
+        }
+
         private static void ReuseCalculation()
         {
             ViewHistory();
@@ -183,7 +255,7 @@ namespace CalculatorLibrary
             Console.WriteLine("2. Cosine (cos)");
             Console.WriteLine("3. Tangent (tan)");
             Console.Write("Choose a function: ");
-            string choice = Console.ReadLine();
+            string? choice = Console.ReadLine();
 
             Console.WriteLine("Enter the angle in degrees:");
             double angle = Convert.ToDouble(Console.ReadLine());
@@ -214,7 +286,7 @@ namespace CalculatorLibrary
             string calculation = $"{function}({angle}°) = {result}";
             calculationHistory.Add(calculation);
             Console.WriteLine($"Result: {result}");
-            Console.WriteLine("Calculation saved to history.");
+            Console.WriteLine("\nCalculation saved to history.");
         }
         static void CalculateTenPowerX()
         {
